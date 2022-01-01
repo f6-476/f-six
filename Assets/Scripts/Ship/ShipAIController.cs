94a7d87017bbfe95a58e1f0ac337642da58d9b42
PipeFlowWizard <elijahmon96@gmail.com>
2021-12-31 20:19:33 -0500

    small update

---
+++ Assets/Scripts/Ship/ShipAIController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Ship))]
+public class ShipAIController : Controller
+{
+    private void Update()
+    {
+        thrustValue = 1f;
+    }
+
+    public void GetBoost(InputAction.CallbackContext ctx)
+    {
+        /*if (ctx.performed)
+        {
+            Debug.Log("boost true");
+            boost = true;
+        }
+        if (ctx.canceled)
+        {
+            Debug.Log("boost false");
+            boost = false;
+        }*/
+    }
+    
+}
