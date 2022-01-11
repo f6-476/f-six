26b280e42d0a763c6ff33d22470c6f5866476397
PipeFlowWizard <elijahmon96@gmail.com>
2022-01-11 17:37:32 -0500

    cleanup/separate track/curve/spline

--- Assets/Scripts/Utility/BezierCurve.cs
+++ Assets/Scripts/Utility/BezierCurve.cs
@@ -9,6 +9,8 @@ using System.Linq;
 public class BezierCurve : MonoBehaviour
 {
     [SerializeField] private Transform[] controlPoints = new Transform[4];
+
+    [SerializeField] private Vector3[] points;
     [Range(0, 1)] public float T = 0.5f;
     [Range(3, 50)] public int resolution = 3;
     [Range(2, 32)] public int edgeRingCount;
@@ -20,7 +22,7 @@ public class BezierCurve : MonoBehaviour
 
     private void OnValidate()
     {
-        vertices = GenerateVertices();
+        //vertices = GenerateVertices();
         //GenerateMesh();
         
     }
@@ -28,17 +30,17 @@ public class BezierCurve : MonoBehaviour
 
     private void Awake()
     {
-        mesh = new Mesh();
-        mesh.name = "Segment";
-        GetComponent<MeshFilter>().sharedMesh = mesh;
-        GetComponent<MeshCollider>().sharedMesh = mesh;
-        vertices = GenerateVertices();
-        GenerateMesh();
+        // mesh = new Mesh();
+        // mesh.name = "Segment";
+        // GetComponent<MeshFilter>().sharedMesh = mesh;
+        // GetComponent<MeshCollider>().sharedMesh = mesh;
+        // vertices = GenerateVertices();
+        // GenerateMesh();
     }
 
     private void Update()
     {
-        GenerateMesh();
+        // GenerateMesh();
     }
 
     public Vector3 GetPos(int i) => controlPoints[i].position;
@@ -78,76 +80,76 @@ public class BezierCurve : MonoBehaviour
 
     }
 
-    public Vector3[] GenerateVertices()
-    {
-        List<Vector3> verts = new List<Vector3>();
-        verts.Clear();
-        for (int i = 0; i < resolution; i++)
-        {
-            var angle = -(Mathf.PI * 2) / resolution;
-            verts.Add(new Vector3(Mathf.Cos(angle * i), Mathf.Sin(angle * i), 0));
-        }
+    // public Vector3[] GenerateVertices()
+    // {
+    //     List<Vector3> verts = new List<Vector3>();
+    //     verts.Clear();
+    //     for (int i = 0; i < resolution; i++)
+    //     {
+    //         var angle = -(Mathf.PI * 2) / resolution;
+    //         verts.Add(new Vector3(Mathf.Cos(angle * i), Mathf.Sin(angle * i), 0));
+    //     }
+    //
+    //     return verts.ToArray();
+    // }
+    //
+    // public void GenerateMesh()
+    // {
+    //     mesh.Clear();
+    //     
+    //     // Vertices
+    //     List<Vector3> verts = new List<Vector3>();
+    //     for (int ring = 0; ring < edgeRingCount; ring++)
+    //     {
+    //         float t = ring / (edgeRingCount - 1f);
+    //
+    //         OrientedPoint op = GetBezierPoint(t);
+    //         
+    //         for (int i = 0; i < vertices.Length; i++)
+    //         {
+    //             verts.Add(op.LocalToWorldPosition(vertices[i] * thickness));
+    //         }
+    //     }
+    //
+    //     // Triangle
+    //     List<int> triangleIndices = new List<int>();
+    //     for (int ring = 0; ring < edgeRingCount - 1; ring++)
+    //     {
+    //         int rootIndex = ring * vertices.Length;
+    //         int rootIndexNext = (ring + 1) * vertices.Length;
+    //
+    //         for (int line = 0; line < vertices.Length; line++)
+    //         {
+    //             int lineIndexA = line;
+    //             int lineIndexB = line == vertices.Length - 1 ? 0 : line + 1;
+    //             
+    //
+    //             int currentA = rootIndex + lineIndexA;
+    //             int currentB = rootIndex + lineIndexB;
+    //             int nextA = rootIndexNext + lineIndexA;
+    //             int nextB = rootIndexNext + lineIndexB;
+    //             
+    //             /*Debug.Log("index currentA: " + currentA);
+    //             Debug.Log("index currentB: " + currentB);
+    //             Debug.Log("index nextA: " + nextA);
+    //             Debug.Log("index nextB: " + nextB);*/
+    //             
+    //             triangleIndices.Add(currentA);
+    //             triangleIndices.Add(nextA);
+    //             triangleIndices.Add(nextB);
+    //             triangleIndices.Add(currentA);
+    //             triangleIndices.Add(nextB);
+    //             triangleIndices.Add(currentB);
+    //
+    //         }
+    //
+    //     }
+    //     mesh.SetVertices(verts);
+    //     mesh.SetTriangles(triangleIndices,0);
+    //     mesh.RecalculateNormals();
+    // }
 
-        return verts.ToArray();
-    }
-    
-    public void GenerateMesh()
-    {
-        mesh.Clear();
-        
-        // Vertices
-        List<Vector3> verts = new List<Vector3>();
-        for (int ring = 0; ring < edgeRingCount; ring++)
-        {
-            float t = ring / (edgeRingCount - 1f);
-
-            OrientedPoint op = GetBezierPoint(t);
-            
-            for (int i = 0; i < vertices.Length; i++)
-            {
-                verts.Add(op.LocalToWorldPosition(vertices[i] * thickness));
-            }
-        }
-   
-        // Triangle
-        List<int> triangleIndices = new List<int>();
-        for (int ring = 0; ring < edgeRingCount - 1; ring++)
-        {
-            int rootIndex = ring * vertices.Length;
-            int rootIndexNext = (ring + 1) * vertices.Length;
-
-            for (int line = 0; line < vertices.Length; line++)
-            {
-                int lineIndexA = line;
-                int lineIndexB = line == vertices.Length - 1 ? 0 : line + 1;
-                
-
-                int currentA = rootIndex + lineIndexA;
-                int currentB = rootIndex + lineIndexB;
-                int nextA = rootIndexNext + lineIndexA;
-                int nextB = rootIndexNext + lineIndexB;
-                
-                /*Debug.Log("index currentA: " + currentA);
-                Debug.Log("index currentB: " + currentB);
-                Debug.Log("index nextA: " + nextA);
-                Debug.Log("index nextB: " + nextB);*/
-                
-                triangleIndices.Add(currentA);
-                triangleIndices.Add(nextA);
-                triangleIndices.Add(nextB);
-                triangleIndices.Add(currentA);
-                triangleIndices.Add(nextB);
-                triangleIndices.Add(currentB);
-
-            }
-
-        }
-        mesh.SetVertices(verts);
-        mesh.SetTriangles(triangleIndices,0);
-        mesh.RecalculateNormals();
-    }
-
-    OrientedPoint GetBezierPoint(float t)
+    public OrientedPoint GetBezierPoint(float t)
     {
         Vector3 p0 = GetPos(0);
         Vector3 p1 = GetPos(1);
