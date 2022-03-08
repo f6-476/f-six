46df72657eb06b4c08216ad92e7d05a7f0f86400
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-03-07 19:50:38 -0500

    Create Game Manager Singleton Instance

---
+++ Assets/Code/Scripts/Race/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
