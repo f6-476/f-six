50cd8c7f3a8011d470a03b9d0e4b273a650f0e65
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-04-10 19:52:24 -0400

    Half-Implement new HUD

--- Assets/Code/Scripts/Ship/ShipView.cs
+++ Assets/Code/Scripts/Ship/ShipView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,38 +12,75 @@ public class ShipView : MonoBehaviour
     [SerializeField] private Text _lapText;
     [SerializeField] private Text _stopWatchText;
     [SerializeField] private Text _timeLapText;
+    [SerializeField] private Text _rankingsText;
 
     public void SetRankText(int rank)
     {
-        _rankText.text = $"RANK: {rank}";
+        if (rank == 1)
+        {
+            _rankText.text = $"{rank}st";
+        }
+        else if (rank == 2)
+        {
+            _rankText.text = $"{rank}nd";
+        }
+        else if (rank == 3)
+        {
+            _rankText.text = $"{rank}rd";
+        }
+        else
+        {
+            _rankText.text = $"{rank}th";
+        }
     }
     
     public void SetLapText(int lap)
     {
-        _lapText.text = $"LAPS: {lap}";
+        _lapText.text = $"{lap}/3 LAPS";
     }
     
     public void SetStopwatchText(float lapTime)
     {
-        _stopWatchText.text = $"LAP TIME: {lapTime}";
+        _stopWatchText.text = SetFloatToTimer(lapTime);
     }
 
     public void SetLapTimeText(float lapTimeDifference)
     {
         if (lapTimeDifference < 0f)
         {
-            _timeLapText.text = $"LAP TIME: +{Mathf.Abs(lapTimeDifference)}";
+            _timeLapText.text = $"+{SetFloatToTimer(lapTimeDifference)}";
             _timeLapText.color = Color.green;
         }
         else if (lapTimeDifference > 0f)
         {
-            _timeLapText.text = $"LAP TIME: -{Mathf.Abs(lapTimeDifference)}";
+            _timeLapText.text = $"-{SetFloatToTimer(lapTimeDifference)}";
             _timeLapText.color = Color.red;
         }
         else
         {
-            _timeLapText.text = $"LAP TIME: {Mathf.Abs(lapTimeDifference)}";
+            _timeLapText.text = $"{SetFloatToTimer(lapTimeDifference)}";
             _timeLapText.color = Color.grey;
         }
+    }
+
+    public void SetRankingsText()
+    {
+        var playerList = GameManager.Instance._playersShips;
+        var textField = "";
+        var rank = 0;
+        foreach (var player in GameManager.Instance._playersShips)
+        {
+            textField += $"{rank + 1}. {player.name}\n";
+        }
+        _rankingsText.text = textField;
+    }
+
+    private string SetFloatToTimer(float timeFloat)
+    {
+        var minutes = Mathf.FloorToInt(timeFloat / 60);
+        var seconds = Mathf.FloorToInt(timeFloat % 60);
+        //(n-(int)n)*1000;
+        var milliseconds = (timeFloat - (int) timeFloat) * 1000f;
+        return $"{minutes}:{seconds}:{(int) milliseconds}";
     }
 }
