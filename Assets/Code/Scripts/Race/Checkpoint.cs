a034501e9cab5f4dc496b780a8842b13779d9eca
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2022-04-14 20:46:24 -0400

    GameManager + CheckPointManager => RaceManager.
    Additionally improved how ranking is done for ships.

--- Assets/Code/Scripts/Race/Checkpoint.cs
+++ Assets/Code/Scripts/Race/Checkpoint.cs
@@ -5,9 +5,5 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        // if this checkpoint is the "next" checkpoint, increment lastCheckpoint on the ship
-        // If this checkpoint is valid and also the last checkpoint the the lap, increment currentLap on the ship
-    }
+    public int index;
 }
