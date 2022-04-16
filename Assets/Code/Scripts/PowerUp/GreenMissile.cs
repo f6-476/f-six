0727a49a133bf5aa1a807dc11d699fd39cf6f566
Ian Lopez <43527915+ianlop@users.noreply.github.com>
2022-04-16 01:46:58 -0400

    Missile Effects

--- Assets/Code/Scripts/PowerUp/GreenMissile.cs
+++ Assets/Code/Scripts/PowerUp/GreenMissile.cs
@@ -2,15 +2,13 @@
 
 public class GreenMissile : Missile
 {
-    public override void FixedUpdate()
-    {}
-
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.TryGetComponent(out Ship ship))
         {
             ship.Movement.SetThrust();
-            Destroy(gameObject);
+            explosionSound.Play();
+            Destroy(gameObject, explosionSound.clip.length);
         }
     }
 }
