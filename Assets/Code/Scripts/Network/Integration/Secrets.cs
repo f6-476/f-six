7a2ecc10fdfe6eef30f70fde066ec75b7b75d36a
Alexandre Lavoie <36684879+alexandre-lavoie@users.noreply.github.com>
2023-08-14 20:18:13 -0400

    Removed secrets.

--- Assets/Code/Scripts/Network/Integration/Secrets.cs
+++ Assets/Code/Scripts/Network/Integration/Secrets.cs
@@ -3,15 +3,14 @@ public static class Secrets
     public static class Twitch
     {
         // App Access Token
-        // n42y1i4frmfy7e1rwvlscqj3tw4sin
-        public const string BOT_OWNER = "antoinepaulinb7"; //The username which was used to generate the OAuth token
-        public const string BOT_ACCESS_TOKEN = "o5qdoj9i4eufb4vz8yapjfo17kcrj1";//"sv1qi38jnxl1nnwh7c26jrd9s404mq";//"fwrcm5ex54gkumqzbegbdw8v3p9kz7";
-        public const string BOT_REFRESH_TOKEN = "qh8ntqnjaqtfehk67voe1gi1pfapze88zqhend8ct2qzluhgm2";
-        public const string BOT_CLIENT_ID = "gp762nuuoqcoxypju8c569th9wz7q5";
+        public const string BOT_OWNER = "X"; // The username which was used to generate the OAuth token
+        public const string BOT_ACCESS_TOKEN = "X";
+        public const string BOT_REFRESH_TOKEN = "X";
+        public const string BOT_CLIENT_ID = "X";
     }
 
     public static class Discord
     {
-        public const string BOT_TOKEN = "OTY2NjQ5NTMxNjgyOTQ3MDg1.YmE0hw.voG55LvX7nEOagF8ZOsttc0DdK4";
+        public const string BOT_TOKEN = "X";
     }
 }
